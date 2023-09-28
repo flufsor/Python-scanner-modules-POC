547edc90368e8de42f42e4e71b3ffd386efe60fb
Tom Goedemé <flufsor@gmail.com>
2023-09-28 08:56:02 +0200

    add scan abstract class

--- scans/ping_scan.py
+++ scans/ping_scan.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 from datetime import datetime
 
@@ -5,16 +6,22 @@ from .scan import Scan
 
 
 class PingScan(Scan):
-    def scan(self):
+    def scan(self) -> None:
+        """
+        Perform a ping scan on the specified target and capture the result in ms.
+        """
         self.start_time = datetime.now()
         try:
             result = subprocess.run(["ping", "-c", "4", self.target], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
             self.output_log = result.stdout
 
-            if "0% packet loss" in self.output_log:
-                self.result = "stable connection"
+            pattern = "r'(?<=rtt min/avg/max/mdev = [0-9.]/)', '([0-9.])'"
+            match = re.search(pattern, self.output_log)
+
+            if match:
+                self.result = match.group(1)
             else:
-                self.result = "unstable connection"
+                self.result = "Ping failed"
 
         except subprocess.CalledProcessError:
             self.result = "Failed to perform test"
--- scans/scan.py
+++ scans/scan.py
@@ -1,7 +1,18 @@
-from abc import ABC, abstractmethod
 import json
+from abc import ABC, abstractmethod
+
 
 class Scan(ABC):
+    """
+    This abstract class represents a scan operation.
+
+    Attributes:
+        target (str): The target IP address or hostname to ping.
+        start_time (datetime): The timestamp when the ping scan operation started.
+        end_time (datetime): The timestamp when the ping scan operation ended.
+        result (str): The formated formated result of the scan
+        output_log (str): The standard output log of the ping command.
+    """
     def __init__(self, target: str):
         self.target = target
         self.start_time = None
@@ -10,11 +21,20 @@ class Scan(ABC):
         self.output_log = None
 
     @abstractmethod
-    def scan(self):
-        pass
+    def scan(self) -> None:
+        """
+        Perform a scan operation on the specified target and capture the result.
+        """
+        ...
 
 class ScanEncoder(json.JSONEncoder):
-    def default(self, obj):
+    """
+    JSON encoder for encoding Scan objects into JSON format.
+    """
+    def default(self, obj: object) -> dict:
+        """
+        Override the default method to handle encoding of Scan objects.
+        """
         if isinstance(obj, Scan):
             return {
                 "target": obj.target,
